a7833aded4d5c440e9c39163bd946c53a9bf96c6
bobgoe <bobgoeree@gmail.com>
2013-12-14 14:26:48 -0800

    Graph!
    Heuj! Grafiek in CEAstart.html gebasseerd op eigen data input.
    Eerste stap met Test.R, zie voor opstarten testworker develop omgeving
    op desktop.
    
    Todo:
    
    Vanuit Patavi correcte data krijgen (Eerst met Test.R een waarde 5 bvb,
    daarna vanuit Cea.R alle correcte data).
    Grafiek op basis van die data maken.

--- R/test.R
+++ R/test.R
@@ -1,1 +1,8 @@
-weights <- 5
+test <- function(params){
+	allowed <- c('test')
+  if(params$method %in% allowed) {
+  	hoi <- 5
+    do.call(paste(hoi))
+  } else {
+    stop("method not allowed")
+}
--- app/js/controllers/CEAstart.js
+++ app/js/controllers/CEAstart.js
@@ -12,7 +12,7 @@ return function($scope){
 	
 	};
 	
-	$scope.method = "smaa";
+	$scope.method = "test";
 	  $scope.input = "{}";
 	
 	  $scope.submit = function(method, input) {
@@ -34,7 +34,75 @@ return function($scope){
 	
 	    task.results.then(successHandler, errorHandler, progressHandler);
 	  };
-	
+	  
+	  var getBobFunctie = _.memoize(function(state) {
+	        var data = {
+	        	"Placebo": [
+	        	  { x:0,
+	        		y: 1
+	        	  },
+	        	  { x:1,
+	          		y: 0
+	          	  },
+	        	  { x:2,
+	          		y: 0
+	          	  },     
+	        	],
+	        	"Blaat": [
+             	  { x:0,
+              		y: 0
+              	  },
+              	  { x:1,
+                	y: 0.2
+                  },
+              	  { x:2,
+                	y: 0.8
+                  },     
+	            ],
+	            "Leuke naam": [
+             	  { x:0,
+             		y: 0
+             	  },
+             	  { x:1,
+               		y: 0.8
+               	  },
+             	  { x:2,
+               		y: 0.2
+               	  },     
+             	],
+	        }
+	        var result = [];
+	        
+	        for(var i in data) {
+	        	var resultLine = {};
+	        	var line = data[i];
+	        	
+	        	resultLine.key = i;
+	        	resultLine.labels = [];
+	        	resultLine.values = [];
+	        	for(var j in line) {
+	        		var value = {};
+	        		value.series = 0;
+	        		value.x = line[j].x;
+	        		value.y = line[j].y;
+	        		resultLine.values.push(value);
+	        		resultLine.labels.push(j);
+	        	}
+	        	
+	        	result.push(resultLine);
+	        }
+	        console.log('dit hebben we gemaakt', result);
+	        return result;
+	      });
+	  
+	  var initialize = function(state) {
+	      var next = _.extend(state, {
+	        bobFunctie: getBobFunctie
+	      });
+	      return run(next);
+	    };
+	    
+	    $scope.bobFunctie = getBobFunctie;
 };
 	
 });
